f35e9c3e2bc6288f64b59158a9fece1f986ffd0e
Slime <96568406+Slimeemils@users.noreply.github.com>
2021-12-28 00:37:47 -0600

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Slime#4204 QBCore Paycheck system'
+
+version '1.0.0'
+
+client_scripts {
+	'client.lua'
+}
+
+server_scripts {
+	'server.lua'	
+}
